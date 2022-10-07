9592b48050a774e0bc9851598bf32f9ba34b19f0
3B117151 <114204306+3B117151@users.noreply.github.com>
2022-10-07 10:10:54 +0800

    Add files via upload

---
+++ Test7.java
@@ -0,0 +1,37 @@
+ import java.util.Scanner;
+
+public class Test7{
+    public static void main (String[] args){
+        int grade;
+        
+        
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter frist integer:");
+        grade = input.nextInt();
+        
+        if (grade <= 100) {
+            System.out.println("");
+        switch (grade/10){
+            case 9:
+            case 10:
+                System.out.println("a");
+                break;
+            case 8:
+                System.out.println("b");
+                break;
+            case 7:
+                System.out.println("c");
+                break;
+            case 6:
+                System.out.println("d");
+                break;
+            default:
+                System.out.println("e");
+                break;
+        }
+        }
+        else {
+            System.out.println("error");
+        }
+    }
+}
